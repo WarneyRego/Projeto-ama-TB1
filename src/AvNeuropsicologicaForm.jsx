f44952780b416c1d4e90e5deb4659e861527afb6
Reinaldo Coelho <165790519+DadosCoelho@users.noreply.github.com>
2024-10-27 21:08:25 -0300

    Create AvNeuropsicologicaForm.jsx

---
+++ src/AvNeuropsicologicaForm.jsx
@@ -0,0 +1,84 @@
+// Aqui é a parte de envio da avaliação neuropsicológica
+
+import React, { useState } from "react";
+import { getFirestore, doc, setDoc } from "firebase/firestore";
+
+const AvNeuropsicologicaForm = () => {
+  const [ra, setRa] = useState("");
+  const [listaTeste, setListaTeste] = useState("");
+  const [resultados, setResultados] = useState("");
+  const [observacaoAdministrativa, setObservacaoAdministrativa] = useState("");
+
+  const db = getFirestore();
+
+  const handleAvNeuropsicologicaSubmit = async (e) => {
+    e.preventDefault();
+
+    const AvNeuropsicologicaData = {
+      listaTeste,
+      resultados,
+      observacaoAdministrativa,
+    };
+
+    try {
+      // O documento é salvo com o RA como ID (NÃO VAI SER ALEATORIO)
+      const AvNeuropsicologicaDocRef = doc(db, "TB3_Avaliação_Neuropsicológica", ra);
+      await setDoc(AvNeuropsicologicaDocRef, AvNeuropsicologicaData);
+      alert("Dados da Avaliação Neuropsicologica salvos com sucesso!");
+      // Limpar os campos após o envio
+      setRa("");
+      setListaTeste("");
+      setResultados("");
+      setObservacaoAdministrativa("");
+    } catch (error) {
+      console.error("Erro ao salvar os dados da Avaliação Neuropsicologica: ", error);
+    }
+  };
+
+  return (
+    <form onSubmit={handleAvNeuropsicologicaSubmit}>
+      <div>
+        <label>RA (Registro Acadêmico):</label>
+        <input
+          type="text"
+          value={ra}
+          onChange={(e) => setRa(e.target.value)}
+          placeholder="RA do paciente"
+          required
+        />
+      </div>
+      <div>
+        <label>Lista de Testes:</label>
+        <input
+          type="text"
+          value={listaTeste}
+          onChange={(e) => setListaTeste(e.target.value)}
+          placeholder="Lista de Testes"
+          required
+        />
+      </div>
+      <div>
+        <label>Resultados:</label>
+        <input
+          type="text"
+          value={resultados}
+          onChange={(e) => setResultados(e.target.value)}
+          placeholder="Resultados"
+          required
+        />
+      </div>
+      <div>
+        <label>Observação Administrativa:</label>
+        <input
+          type="text"
+          value={observacaoAdministrativa}
+          onChange={(e) => setObservacaoAdministrativa(e.target.value)}
+          placeholder="Observação Administrativa"
+        />
+      </div>
+      <button type="submit">Salvar Avaliação Neuropsicológica</button>
+    </form>
+  );
+};
+
+export default AvNeuropsicologicaForm;
