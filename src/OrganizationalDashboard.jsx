465ce567bc0229abdc2fd532560d8a7fb6a23da8
WarneyRego <compradordecanela@gmail.com>
2024-10-28 23:15:58 +0000

    feat:org-dashboard

--- src/OrganizationalDashboard.jsx
+++ src/OrganizationalDashboard.jsx
@@ -1,44 +1,71 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import Navbar from "./elements/Navbar";
+import "./OrganizationalDashboard.css";
+import { useNavigate } from "react-router-dom";
 const OrganizationalDashboard = () => {
+
+
+
+const navigate = useNavigate(); 
+
+const handleViewAnamnese = () => {
+  navigate("/admin/view-anamnese");
+};
+
+const handleViewAvNeuropsicologica = () => {
+  navigate("/admin/view-avNeuropsicologica");
+};
+
+const handleFamilyUserList = () => {
+  navigate("/organizational/family-users-list");
+};
+
+const HandleAnamneseForm = () => {
+  navigate("/organizational/anamnese-form");
+};
+const HandleFormNeuropsicologica = () => {
+  navigate("/organizational/avNeuropsicologica-form");
+};
+
+const HandleFamilyList = () => {
+  navigate("/organizational/family-users-list");
+};
+
   return (
-    <div>
-      <h1>Painel do Usuário Organizacional</h1>
-      <nav>
-        <ul>
+    <>
+      <Navbar />
+      <nav>  <h1>Painel do Usuário Organizacional</h1></nav>
+      <div className="aroy">
 
-          <li>
-          <Link to="/admin/view-anamnese">Ver Anamnese</Link>
-          </li>
-          <li>
-            <Link to="/organizational/anamnese-form">
-              Ir para Formulário de Anamnese
-            </Link>
-          </li>
 
-          <li>
-            <Link to="/organizational/family-users-list">
+
+        
+        <div onClick={handleViewAnamnese} className="card">
+         view anamnese
+        </div>
+        <div onClick={HandleAnamneseForm } className="card">
+         
+             form
+          
+        </div>
+        <div onClick={handleFamilyUserList} className="card">
               Ir para Lista de Usuários Familiares
-            </Link>
-          </li>
-          <li>
-          <Link to="/admin/view-avNeuropsicologica">Ver AvNeuropsicologica</Link>
-          </li>
-          <li>
-            <Link to="/organizational/avNeuropsicologica-form">
+        </div>
+        <div onClick={handleViewAvNeuropsicologica} className="card">
+          Ver AvNeuropsicologica
+        </div>
+        <div onClick={HandleFormNeuropsicologica} className="card">
+
               Ir para Formulário de AvNeuropsicologica
-            </Link>
-          </li>
-          <li>
-            <Link to="/organizational/family-users-list">
-              Ir para Lista de Usuários Familiares
-            </Link>
-          </li>
+           
+        </div>
+     
 
-        </ul>
-      </nav>
-    </div>
+
+
+      </div>
+    </>
   );
 };
 
