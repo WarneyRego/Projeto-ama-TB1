266a99ea13c1c5fa2b5cef65002028761222b5cf
Reinaldo Coelho <165790519+DadosCoelho@users.noreply.github.com>
2024-10-27 21:08:13 -0300

    Update AdminDashboard.jsx

--- src/AdminDashboard.jsx
+++ src/AdminDashboard.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useNavigate } from "react-router-dom"; 
 import "./estilização/AdminDashboard.css"
 import edicao from "./assets/edicao.png"
+import edicaoTb3 from "./assets/edicaoTb3.jpeg"
 import cadastro from './assets/cadaster.png'
 import user from  './assets/user.png'
 import Navbar from "./elements/Navbar";
@@ -11,6 +12,10 @@ const AdminDashboard = () => {
 
   const handleViewAnamnese = () => {
     navigate("/admin/view-anamnese");
+  };
+
+  const handleViewAvNeuropsicologica = () => {
+    navigate("/admin/view-avNeuropsicologica");
   };
 
   const handleRegisterUser = () => {
@@ -33,6 +38,10 @@ const AdminDashboard = () => {
       <div onClick={handleViewAnamnese} className="card">
         <img src={edicao} alt="Edição de anamnese" />
  
+      </div>
+      <div onClick={handleViewAvNeuropsicologica} className="card">
+        <img src={edicaoTb3} alt="Edição de Avaliação Neuropsicológica" />
+
       </div>
       <div onClick={handleRegisterUser} className="card">
         <img className="cadastro_foto" src={cadastro} alt="Cadastro" />
