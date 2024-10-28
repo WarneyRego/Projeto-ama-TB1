42009aadf2824828626a09eabcc6b1ff32d1d733
Reinaldo Coelho <165790519+DadosCoelho@users.noreply.github.com>
2024-10-27 21:08:18 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AdminDashboard from "./AdminDashboard";
 import RegisterUser from "./RegisterUser";
 import ViewAnamnese from "./ViewAnamnese";
+import ViewAvNeuropsicologica from "./ViewAvNeuropsicologica";
 import EditUser from "./EditUser";
 import OrganizationalDashboard from "./OrganizationalDashboard"; 
 import FamilyDashboard from "./FamilyDashboard"; 
 import Login from "./Login";
 import AnamnesePage from "./AnamnesePage"; 
+import AvNeuropsicologicaPage from "./AvNeuropsicologicaPage"
 import FamilyUsersPage from "./FamilyUsersPage";
 import FamilyUsersList from "./FamilyUsersList";
 import FamilyUserDetail from  "./FamilyUserDetail";
@@ -29,11 +31,13 @@ function App() {
         <Route path="/admin-dashboard" element={<AdminDashboard />} />
         <Route path="/admin/register-user" element={<RegisterUser />} />
         <Route path="/admin/view-anamnese" element={<ViewAnamnese />} />
+        <Route path="/admin/view-avNeuropsicologica" element={<ViewAvNeuropsicologica />} />
         <Route path="/admin/edit-user" element={<EditUser />} />
 
         {/* Rota da dashboard organizacional */}
         <Route path="/organizational-dashboard" element={<OrganizationalDashboard />} />
         <Route path="/organizational/anamnese-form" element={<AnamnesePage />} />
+        <Route path="/organizational/avNeuropsicologica-form" element={<AvNeuropsicologicaPage />} />
         <Route path="/organizational/family-users-list" element={<FamilyUsersPage />} />
         <Route path="/family-user/:id" element={<FamilyUserDetail />} />
         <Route path="/organizational/anamnese/:id" element={<ViewAnamnese />} /> {/* Rota para visualizar a anamnese */}
